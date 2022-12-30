d06b80402e7ae3e7581b38ee6a95f04be4a1b935
K0rgana <anagrom1999@gmail.com>
2022-12-29 21:30:51 -0300

    add mock values

---
+++ mqtt_mock.py
@@ -0,0 +1,50 @@
+import time
+import paho.mqtt.client as paho
+from paho import mqtt
+
+import random
+import time
+
+brokerUrl = "95b18c726f0e4dd0a4df08c8be520675.s2.eu.hivemq.cloud"
+brokerPort = 8883
+brokerUsername = "@lunoifp3"
+brokerPassword = "@lunoifp3"
+
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+client.username_pw_set(brokerUsername, brokerPassword)
+client.connect(brokerUrl, brokerPort)
+
+def mock_number(min, max): 
+  return random.randrange(min,max)
+
+def mock_boolean(): 
+  list = ['false', 'true']
+  return random.choice(list)
+
+def mock_topic(): 
+  list = ['temperature', 'presence']
+  return random.choice(list)
+
+def mock_msgs(max):
+  i=0
+  payload = []
+  while (i<max):
+      topic = mock_topic()
+      if topic == 'temperature':
+        temp = mock_number(17, 30)  
+        msg = {'topic':'lab/temperature', 'payload': temp}
+      if topic == 'presence':
+        boo = mock_boolean()  
+        msg = {'topic':'lab/presence', 'payload': boo}
+      payload.append(msg)
+      i=i+1
+  return payload
+
+ttl = mock_number(5, 15)
+msgs = mock_msgs(ttl)
+for el in msgs:
+  time.sleep(5)
+  #client.publish("lab/temperature", payload="21", qos=1, retain=True)
+  client.publish(el["topic"], payload=el["payload"], qos=1, retain=True)
+  print(el)
